759b3c785b0024ac2cc3fa8f47488aa8fa14d964
Harsh-Vaibhav <harshvaibhav2307@gmail.com>
2025-07-17 10:57:05 +0530

    updated

--- server.js
+++ server.js
@@ -12,20 +12,20 @@ app.use(express.json());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
-  app.listen(8080, () => {
-    console.log("Server started");
-  });
-});
-
-// mongoose
-//   .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.tryowsz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-//   .then(() => {
-//     app.listen(8080, () => {
-//       console.log("Server started");
-//     });
+// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
 //   });
+// });
 
+mongoose
+  .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.tryowsz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
+  });
+  
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter)
